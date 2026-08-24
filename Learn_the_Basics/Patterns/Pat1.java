5fca8bb6522474d688eeb99a3285bdfe57214baf
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-24 16:47:41 +0530

    Add Java gitignore

---
+++ Learn_the_Basics/Patterns/Pat1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pat1{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the no :");
+        int n =sc.nextInt();
+
+        // for (int i =1; i<=n;i++){
+        //     for (int j=1; j<=i; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for (int i=1; i<=n; i++){
+        //     for (int j=1; j<=i; j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        for (int i=1; i<=n;i++){
+            for(int j=1; j<=n+1-i;j++){
+                    System.out.print("*");
+
+            }
+            System.out.println();
+                
+        }
+            
+    }
+}
