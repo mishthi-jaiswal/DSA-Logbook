000fe8eb54b0fffca34899b60b767525c4a7fec4
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-27 03:20:15 +0530

    Simplified code for  pattern 15

--- Learn_the_Basics/Patterns/Pat15.java
+++ Learn_the_Basics/Patterns/Pat15.java
@@ -11,8 +11,12 @@ public class Pat15 {
         
         for (int i=n; i>=1; i--){
             
-            for (int j=1 ; j<=i; j++){
-                System.out.print((char)(64+j));
+            // for (int j=1 ; j<=i; j++){
+            //     System.out.print((char)(64+j));
+                
+            // }
+            for (char c= 'A' ; c<'A'+i; c++){
+                System.out.print(c);
                 
             }
             System.out.println();
