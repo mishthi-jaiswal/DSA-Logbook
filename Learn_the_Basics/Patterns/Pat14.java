3dc5a7478e2484f775fa2f2d45f4a904cfb1fc93
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-27 03:17:16 +0530

    Simplified code for  pattern 14

--- Learn_the_Basics/Patterns/Pat14.java
+++ Learn_the_Basics/Patterns/Pat14.java
@@ -11,9 +11,12 @@ public class Pat14 {
         
         for (int i=1; i<=n; i++){
             
-            for (int j=1 ; j<=i; j++){
-                System.out.print((char)(64+j));
-                
+            // for (int j=1 ; j<=i; j++){
+            //     System.out.print((char)(64+j));
+            // }
+
+            for (char c='A'; c<'A'+i; c++){
+                System.out.print(c);
             }
             System.out.println();
            
