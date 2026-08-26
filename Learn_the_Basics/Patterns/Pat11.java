3c08a21a56ed7b2ed1cfac96947148e42475d2d9
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-27 03:00:08 +0530

    Added striver's method for pattern 11

--- Learn_the_Basics/Patterns/Pat11.java
+++ Learn_the_Basics/Patterns/Pat11.java
@@ -6,17 +6,36 @@ public class Pat11 {
         System.out.print("Enter the no :");
         int n= sc.nextInt();
         
+        //METHOD 1: MY METHOD 
+        // int r= 1;
+        // for (int i=1; i<=n; i++){
+        //     int start = r;
+        //     for (int j=1 ; j<=i; j++){
+        //         System.out.print(start +" ");
+        //         start^=1;
+        //     }
+        //     System.out.println();
+        //     r^=1;
+            
+        // }
 
-        int r= 1;
+
+        //METHOD 2: STRIVER'S METHOD
+
+        //even i starts with 0 and odd i starts with 1
+        int start;
         for (int i=1; i<=n; i++){
-            int start = r;
-            for (int j=1 ; j<=i; j++){
-                System.out.print(start +" ");
-                start^=1;
+            if (i%2==0){
+                start= 0;
+            }
+            else{
+                start=1;
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print(start);
+                start=1-start;  //1-0=1 and 1-1=0
             }
             System.out.println();
-            r^=1;
-            
         }
         
     }
