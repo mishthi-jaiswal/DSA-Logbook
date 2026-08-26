ce089f6f6f9bb85100cf4fa5d52988e417ccae82
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-27 03:34:09 +0530

    Alternate code for  pattern 16

--- Learn_the_Basics/Patterns/Pat16.java
+++ Learn_the_Basics/Patterns/Pat16.java
@@ -8,16 +8,25 @@ public class Pat16 {
         
         
 
-        
-        for (int i=1; i<=n; i++){
+        //METHOD 1: MY WAY
+        // for (int i=1; i<=n; i++){
             
-            for (int j=1 ; j<=i; j++){
-                System.out.print((char)(64+i));
+        //     for (int j=1 ; j<=i; j++){
+        //         System.out.print((char)(64+i));
                 
-            }
-            System.out.println();
+        //     }
+        //     System.out.println();
            
             
+        // }
+
+        //METHOD 2
+
+        for (char c='A'; c<'A'+n; c++){
+            for (int j=1; j<=c-64; j++){
+                System.out.print(c);
+            }
+            System.out.println();
         }
         
     }
