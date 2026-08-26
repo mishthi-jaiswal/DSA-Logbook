fb8f59afd7f1247e84f46c5604a83547996237b3
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-27 02:31:08 +0530

    Added striver's method

--- Learn_the_Basics/Patterns/Pat10.java
+++ Learn_the_Basics/Patterns/Pat10.java
@@ -5,20 +5,57 @@ public class Pat10 {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter the no :");
         int n= sc.nextInt();
+        
 
+        //METHOD 1 
 
-        //increasing pattern
-        for (int i=1; i<=n; i++){
-            for (int j=1 ; j<=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        // //increasing pattern
+        // for (int i=1; i<=n; i++){
+        //     for (int j=1 ; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
 
             
-        }
-        //decreasing pattern
-        for (int i=n-1 ; i>=1; i--){
-            for (int j=1; j<=i; j++){
+        // }
+        // //decreasing pattern
+        // for (int i=n-1 ; i>=1; i--){
+        //     for (int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // METHOD 2 : MATRIX DIAGONAL FORMULAE
+        // //increasing pattern
+        // for (int i=1; i<=n; i++){
+        //     for (int j=1 ; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+            
+        // }
+        // //decreasing pattern
+        // for (int i=1; i<=n-1; i++){
+        //     for (int j=1 ; j<=n-i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+            
+        // }
+
+        // METHOD 3: STRIVER'S METHOD
+
+        for(int i = 1; i<=2*n-1; i++){
+            int stars=i;
+            if (i>n){
+                stars=2*n-i;
+            }
+
+            for (int j=1; j<=stars; j++){
                 System.out.print("*");
             }
             System.out.println();
