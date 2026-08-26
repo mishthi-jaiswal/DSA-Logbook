54ac34ef9bd74d56e06b8ad5d4a875108b272e07
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-26 16:30:56 +0530

    Add pattern 17

---
+++ Learn_the_Basics/Patterns/Pat17.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Pat17 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the no :");
+        int n= sc.nextInt();
+        
+        
+
+        
+        for (int i=1; i<=n; i++){
+            
+            for (int j=1 ; j<=n-i; j++){
+                System.out.print(" ");
+                
+            }
+           
+
+            for (int j=1; j<=i; j++){
+                System.out.print((char)(64+j));
+            }
+            for (int j=i-1; j>=1; j--){
+                System.out.print((char)(64+j));
+            }
+            
+            System.out.println();
+
+            
+        }
+           
+            
+        
+        
+    }
+    
+}
