49eae4efddff2b05d5097d2774d5135056d0ed84
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-08-26 15:54:10 +0530

    Add pattern 12

---
+++ Learn_the_Basics/Patterns/Pat12.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pat12 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the no :");
+        int n= sc.nextInt();
+        int x= 2*n; //no of columns
+        
+
+        
+        for (int i=1; i<=n; i++){
+            
+            for (int j=1 ; j<=i; j++){
+                System.out.print(j);
+                
+            }
+            for (int j=i+1; j<=x-i; j++){
+                System.out.print(" ");
+            }
+            for (int j =x-i+1; j<=x; j++){
+                System.out.print(x-j+1);
+            }
+            System.out.println();
+            
+        }
+        
+    }
+    
+}
