3d2534a8bf912a41e5637d5cc9bfc0d196853bf2
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-01 23:16:40 +0530

    Recursion :Print a name n times

---
+++ Learn_the_Basics/Basic_Recursion/print_names_n_times.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_name(int i, int n){
+    if(i>n) return; //base condition
+    cout<<"Mishthi"<<endl;
+    print_name(++i,n);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the no :";
+    cin>>n;
+
+    print_name(1,n);//learning change of parameters
+    return 0;
+}
