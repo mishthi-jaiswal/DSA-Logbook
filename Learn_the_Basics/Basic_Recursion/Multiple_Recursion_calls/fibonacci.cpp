ce0000cadbe8510c9d7baf9b7752387aae1219ba
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-03 19:16:32 +0530

    Fibonacci : using loop +array

---
+++ Learn_the_Basics/Basic_Recursion/Multiple_Recursion_calls/fibonacci.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int f(int n){
+
+// }
+
+int main(){
+    int n; 
+    cout<<"Enter a number :";
+    cin>>n;
+
+
+    //METHOD 1 : USING FOR LOOP
+    // int first=0;
+    // int second =1;
+    // int ans=0;
+
+    // if (n==0) ans=0;
+    // else if (n==1) ans=1;
+    // else{
+    //     for (int i=2;i<=n; i++){
+
+    //     ans=first+second;
+    //     first=second;
+    //     second=ans;
+    //     }
+
+    // }
+    // cout<<ans;
+
+
+    //METHOD 2: USING LOOP +ARRAY
+    int a[n+1];
+    a[0]=0;
+    a[1]=1;
+    
+
+    for(int i=2; i<=n; i++){
+        a[i]=a[i-1]+a[i-2];
+
+    }
+    cout<<a[n];
+    
+
+
+    //fibonacci : 0 1 1 2 3 5 8 ..... f(0)=0, f(1)=1, f(2)=1, f(3)=2, f(4)=3, f(5)=5
+    //f(n) means print the nth fibonacci no
+    // cout<<f(n);
+    return 0;
+}
