15d848f09857c6299f1e71f85993058fd943163a
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-03 19:40:48 +0530

    Fibonacci : Multiple recursion

--- Learn_the_Basics/Basic_Recursion/Multiple_Recursion_calls/fibonacci.cpp
+++ Learn_the_Basics/Basic_Recursion/Multiple_Recursion_calls/fibonacci.cpp
@@ -1,10 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// int f(int n){
+//METHOD 3:MULTIPLE RECURSION
+int f(int n){
+    if (n<=1) return n;
 
-// }
+    return f(n-1)+f(n-2);
+} //TIME COMPLEXITY= O(2^n) ....exponential
 
+//fibonacci : 0 1 1 2 3 5 8 ..... f(0)=0, f(1)=1, f(2)=1, f(3)=2, f(4)=3, f(5)=5 .....0 based indexing
+//f(n) means print the nth fibonacci no
 int main(){
     int n; 
     cout<<"Enter a number :";
@@ -31,21 +36,20 @@ int main(){
 
 
     //METHOD 2: USING LOOP +ARRAY
-    int a[n+1];
-    a[0]=0;
-    a[1]=1;
+    // int a[n+1];
+    // a[0]=0;
+    // a[1]=1;
     
 
-    for(int i=2; i<=n; i++){
-        a[i]=a[i-1]+a[i-2];
+    // for(int i=2; i<=n; i++){
+    //     a[i]=a[i-1]+a[i-2];
 
-    }
-    cout<<a[n];
+    // }
+    // cout<<a[n];
     
 
 
-    //fibonacci : 0 1 1 2 3 5 8 ..... f(0)=0, f(1)=1, f(2)=1, f(3)=2, f(4)=3, f(5)=5
-    //f(n) means print the nth fibonacci no
-    // cout<<f(n);
+    
+    cout<<f(n);
     return 0;
 }
