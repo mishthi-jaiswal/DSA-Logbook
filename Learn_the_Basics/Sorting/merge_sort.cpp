a8b8f75fde2f66fc110c416cd7d2e4c8c7469314
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-08 02:28:38 +0530

    Merge sort :DESC

--- Learn_the_Basics/Sorting/merge_sort.cpp
+++ Learn_the_Basics/Sorting/merge_sort.cpp
@@ -3,15 +3,71 @@
 using namespace std;
 
 //ASCENDING ORDER
+// void merge(int a[], int low, int mid , int high){
+//      int temp[high-low +1];
+//      int k=0; //index pointer for temp array
+//      int left=low;
+//      int right= mid+1;
+
+//      //compare the 1 element from each part of the 2 arrays and add the smaller one to the temp 
+//      while(left<=mid && right<=high){
+//         if (a[left]<=a[right]){
+//             temp[k]=a[left];
+//             left++;
+//             k++;
+
+//         }
+//         else{
+//             temp[k]=a[right];
+//             right++;
+//             k++;
+//         }
+//      }
+
+//      //if one of the array finishes the while loop breaks but the second array elements have to be inserted as it is
+//      while (left<=mid){
+//         temp[k]=a[left];
+//         left++;
+//         k++;
+//      }
+//      while (right<=high){
+//         temp[k]=a[right];
+//         right++;
+//         k++;
+//      }
+
+//      //now transfer the sorted temp elements to the original array
+//      for (int i=low; i<=high; i++){
+//         a[i]=temp[i-low];
+//      }
+
+// }
+
+// void merge_sort(int a[], int low, int high){
+//     //base case: when array length is 1
+//     if(low>=high){ //we have put > just for safety ,ow original condition is low==high
+//         return;
+//     }
+
+//     //work
+//     int mid = (low+high)/2;
+//     merge_sort(a,low, mid);
+//     merge_sort(a,mid +1, high);
+//     merge(a, low, mid, high);//prodces a merged array which is sorted (and it is done in place)
+// }
+
+
+
+//DESCENDING ORDER
 void merge(int a[], int low, int mid , int high){
      int temp[high-low +1];
      int k=0; //index pointer for temp array
      int left=low;
      int right= mid+1;
 
-     //compare the 1 element from each part of the 2 arrays and add the smaller one to the temp 
+     //compare the 1 element from each part of the 2 arrays and add the LARGER one to the temp 
      while(left<=mid && right<=high){
-        if (a[left]<=a[right]){
+        if (a[left]>=a[right]){  //THE ONLY CHANGE FOR DESCENDING ORDER
             temp[k]=a[left];
             left++;
             k++;
