fa3068789e9c5534e6a51b8108c4653c95fd1ccc
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-09 01:15:51 +0530

    Better solution : 2 loops

--- L3_Arrays/second_largest_element.cpp
+++ L3_Arrays/second_largest_element.cpp
@@ -44,20 +44,37 @@ int main(){
     }
 
     //BRUTE FORCE: sort the array and LARGEST=last element of array ; Time= O(N log N)
-    quick_sort(a,0,n-1);
+    // quick_sort(a,0,n-1);
 
-    int maxi=a[n-1];
-    int flag =false;
-    for(int i=n-2; i>=0; i--){
-        if (a[i]<maxi){
-            cout<<"Second Largest :"<<a[i];
-            flag=true;
-            break;
+    // int maxi=a[n-1];
+    // int flag =false;
+    // for(int i=n-2; i>=0; i--){
+    //     if (a[i]<maxi){
+    //         cout<<"Second Largest :"<<a[i];
+    //         flag=true;
+    //         break;
+    //     }
+    // }
+    // if(!flag){
+    //     cout<<"no second largest element!";
+    // }
+
+
+    //BETTER SOLUTION : 2 LOOPS ; TIME: O(2N)
+    int largest=a[0];
+    for (int i=0; i<n; i++){
+        if (a[i]>largest){
+            largest=a[i];
         }
     }
-    if(!flag){
-        cout<<"no second largest element!";
+
+    int second_largest=INT_MIN;
+    for (int i=0; i<n; i++){
+        if(a[i]<largest && a[i]>second_largest){
+            second_largest=a[i];
+        }
     }
+    cout<<"Second Largest : "<<second_largest;
 
 
     return 0;
