563d2bab7de43b1282c7c351b25d7d0cf1e9e68c
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-09 01:06:08 +0530

    Brute force

---
+++ L3_Arrays/second_largest_element.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int a[], int low, int high){
+    int pivot=a[low];
+    int i=low+1;
+    int j=high;
+
+    while(i<=j){
+        while(i<=high && a[i]<=pivot){
+            i++;
+        }
+        while(j>=low+1 && a[j]>pivot){
+            j--;
+        }
+        if (i<j){
+            swap(a[i],a[j]);
+        }
+    }
+
+    swap(a[low],a[j]);
+    return j;
+}
+
+void quick_sort(int a[], int low, int high){
+    if(low<high){
+        int pivot_index=partition(a, low, high);
+        quick_sort(a,low, pivot_index-1);
+        quick_sort(a,pivot_index+1, high);
+
+    }
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter the no of elements in array :";
+    cin>>n;
+
+    cout<<"Enter the array elements :";
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    //BRUTE FORCE: sort the array and LARGEST=last element of array ; Time= O(N log N)
+    quick_sort(a,0,n-1);
+
+    int maxi=a[n-1];
+    int flag =false;
+    for(int i=n-2; i>=0; i--){
+        if (a[i]<maxi){
+            cout<<"Second Largest :"<<a[i];
+            flag=true;
+            break;
+        }
+    }
+    if(!flag){
+        cout<<"no second largest element!";
+    }
+
+
+    return 0;
+}
