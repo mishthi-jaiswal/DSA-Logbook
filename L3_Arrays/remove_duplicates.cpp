9789970ccc7c53d3f7f262ce3f38ade1e46e21c2
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-09 20:02:21 +0530

    Brute force

---
+++ L3_Arrays/remove_duplicates.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    //input array
+    int n;
+    cout<<"Enter the no of elements in array :";
+    cin>>n;
+
+    cout<<"Enter the array elements :";
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    //REMOVE DUPLICATES FROM A SORTED ARRAY 
+    //WORK: to modify the array and return no of unique elements
+
+
+
+    //BRUTE FORCE: using set ; TIME=O(N log N) SPACE=O(N)
+    set <int> s;
+    for (auto it : a){
+        s.insert(it);
+    }
+
+    int i=0;
+    for(auto it : s){
+        a[i]=it;
+        i++;
+    }
+    cout<<"No of unique elements :"<<i<<endl;
+
+
+    //print array
+    for (auto it : a){
+        cout<<it<<" ";
+    }
+    
+    return 0;
+}
