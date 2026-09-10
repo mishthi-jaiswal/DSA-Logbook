186a345fc82ebd786f715d5be3f1ceb91e98f4fc
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-10 22:45:29 +0530

    BRUTE FORCE

--- L3_Arrays/left_rotate_by_D_placeS.cpp
+++ L3_Arrays/left_rotate_by_D_placeS.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main(){
+    //input array
     int n;
     cout<<"Enter length of array : ";
     cin>>n;
-
     cout<<"Enter array : ";
     int a[n];
     for(int i=0;i<n; i++){
@@ -14,26 +14,58 @@ int main(){
 
 
     //left rotate by D places
-    //time: O(n) and space:O(d)
+    
+
+    //STRIVER'S SOLUTION
+    //time: O(n +d) and space:O(d)
     int d;
     cout<<"Enter the no : ";
     cin>>d;
     d=d%n;//Remember to handle d>n
 
-    
-    vector<int> v(a, a+d);
+    vector<int> v(a, a+d);  //Time : O(d)
+   
 
-    for (int i=0; i<n-d; i++){
-        a[i]=a[i+d];
+    //filling the front positions
+    for (int i=d; i<n;i++){  //TIME : O(n-d)
+        a[i-d]=a[i];
     }
+
+
+    //filing back positions
+    for (int i=n-d; i<n; i++){//Iime : O(d)
+        a[i]=v[i-(n-d)];
+    }
+
+    //overall Time=O(n+d)
+
+
+
+
+
+
     
-    int j=0;
-    int k=n-d;
-    while(j<d){
-        a[k]=v[j];
-        k++;
-        j++;
-    }
+
+    //MY SOLUTION
+    // int d;
+    // cout<<"Enter the no : ";
+    // cin>>d;
+    // d=d%n;//Remember to handle d>n
+
+    
+    // vector<int> v(a, a+d);
+
+    // for (int i=0; i<n-d; i++){
+    //     a[i]=a[i+d];
+    // }
+    
+    // int j=0;
+    // int k=n-d;
+    // while(j<d){
+    //     a[k]=v[j];
+    //     k++;
+    //     j++;
+    // }
 
     //print array
     for (auto it :a){
