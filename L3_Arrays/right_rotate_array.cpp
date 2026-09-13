b0a5cc4b39c2b736a3ed4bd37344d90059d84af2
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-14 00:39:54 +0530

    Brute force

--- L3_Arrays/right_rotate_array.cpp
+++ L3_Arrays/right_rotate_array.cpp
@@ -15,15 +15,38 @@ int main(){
     }
 
 
-    //OPTIMAL SOLUTION
+    //BRUTE FORCE
+    //Overall time :O(n+d)
+    //space: O(d)
     int d;
-    cout<<"Enter the no : ";
+    cout<< "Enter d : ";
     cin>>d;
-    d=d%n;//Remember to handle d>n
 
-    reverse(a,a+n);    //time =O(n) 
-    reverse(a,a+d);    //time =O(d)
-    reverse(a+d,a+n);  //time =O(n-d)
+    d=d%n;
+    vector <int> v;
+    for (int i=n-d; i<n; i++){
+        v.push_back(a[i]);
+    }
+
+    for (int i=n-d-1; i>=0; i--){
+        a[i+d]=a[i];
+    }
+
+    for (int i=0; i<d; i++){
+        a[i]=v[i];
+    }
+
+
+
+    //OPTIMAL SOLUTION
+    // int d;
+    // cout<<"Enter the no : ";
+    // cin>>d;
+    // d=d%n;//Remember to handle d>n
+
+    // reverse(a,a+n);    //time =O(n) 
+    // reverse(a,a+d);    //time =O(d)
+    // reverse(a+d,a+n);  //time =O(n-d)
 
     //Overall time :O(2n)
     //space: O(1)
