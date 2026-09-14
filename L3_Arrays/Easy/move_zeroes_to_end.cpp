e136b02e650e8742659ea181834d8271a0541f73
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-14 19:38:23 +0530

    optimal solution

--- L3_Arrays/Easy/move_zeroes_to_end.cpp
+++ L3_Arrays/Easy/move_zeroes_to_end.cpp
@@ -1,6 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+    //OPTIMAL SOLUTION:SWAP
+    //time=O(n) and space=O(1)
+
+    //1st find the index of 1st zero
+    void f(int a[],int n){
+        int j=-1;
+        for (int i=0;i<n;i++){
+            if (a[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return; //no zeroes found;
+
+        //now do swap
+        for (int i=j+1; i<n; i++){
+            if (a[i]!=0){
+                swap(a[j],a[i]);
+                j++; //now zero is here
+            }
+        }
+
+
+    }
+    
+
 int main(){
     //input array
     int n;
@@ -12,23 +38,42 @@ int main(){
         cin>>a[i];
     }
 
+    f(a,n);
+
+
+
+    //MY SOLUTION:
+    //time=O(2n)
+    //space=O(1)....better than brute force
+    // int i=-1;
+    // for (int j=0; j<n; j++){
+    //     if(a[j]!=0){
+    //         a[i+1]=a[j];
+    //         i++;
+    //     }
+    // }
+
+    // for (int k=i+1; k<n; k++){
+    //     a[k]=0;
+    // }
+
 
     //BRUTE FORCE SOLUTION
-    vector<int> v;
-    for(auto it : a){
-        if (it!=0){
-            v.push_back(it);
-        }
-    }
+    // vector<int> v;
+    // for(auto it : a){
+    //     if (it!=0){
+    //         v.push_back(it);
+    //     }
+    // }
 
-    for (int i=0; i<v.size(); i++){
-        a[i]=v[i];
-    }
-    for(int i=v.size(); i<n;i++){
-        a[i]=0;
-    }
-    //Overall time :O(n)
-    //space: O(n)
+    // for (int i=0; i<v.size(); i++){
+    //     a[i]=v[i];
+    // }
+    // for(int i=v.size(); i<n;i++){
+    //     a[i]=0;
+    // }
+    //Overall time :O(2n)
+    //space: O(n-k)=O(n)
    
 
 
