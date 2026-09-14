bfecbe6b84ff90d53f57f065dc2a385fdc1ecbc8
mishthi-jaiswal <mishthijaiswal211@gmail.com>
2026-09-14 22:12:26 +0530

    optimal

--- L3_Arrays/Easy/union_of_sorted_arrays.cpp
+++ L3_Arrays/Easy/union_of_sorted_arrays.cpp
@@ -23,20 +23,66 @@ int main(){
         cin>>b[i];
     }
 
-    //union 
+    //OPTIMAL
+        vector<int> v;
+    
+    int i=0;
+    int j=0;
+    while(i<n1 && j<n2){
+        if (a[i]<=b[j]){
+            if(v.size()==0 || a[i]!=v.back()){
+                 v.push_back(a[i]);
+            }
+            i++; 
+            
+        }
+        
+        else {
+            if (v.size()==0 || b[j]!=v.back()){
+                v.push_back(b[j]);
+            }
+            j++;
+        }
+        
+    }
+    while(i<n1){
+        
+        if(v.size()==0 || a[i]!=v.back()){
+            v.push_back(a[i]);
+        }
+        i++;
+    
+    }
+    while(j<n2){
+        if (v.size()==0 || b[j]!=v.back()){
+                v.push_back(b[j]);
+            
+            j++;
+        }
+    }
+    for (auto it: v){
+        cout<<it<<" ";
+    }
+    //TIME : O(n1+n2)
+    //SPACE : O(n1+n2)
+
+
+
+
+
 
 
     //BRUTE FORCE
-    set<int> u;
-    for (int i=0; i<n1;i++){//time : O(n1 log u)
-        u.insert(a[i]);
-    }
-    for (int i=0; i<n2;i++){//time : O(n2 log u)
-        u.insert(b[i]);
-    }
-    for(auto it : u){  //time: O(u).....u is no of unique elements ...worst u=n1+n2
-        cout<<it<<" ";
-    }
+    // set<int> u;
+    // for (int i=0; i<n1;i++){//time : O(n1 log u)
+    //     u.insert(a[i]);
+    // }
+    // for (int i=0; i<n2;i++){//time : O(n2 log u)
+    //     u.insert(b[i]);
+    // }
+    // for(auto it : u){  //time: O(u).....u is no of unique elements ...worst u=n1+n2
+    //     cout<<it<<" ";
+    // }
 
     //time :add all
     //space : O(n1+n2)
